c363d27f5ed907085deb662538cfeaee78b97c2a
OKBenzene02 <liyakhatyousuf@gmail.com>
2025-04-13 15:21:39 +0530

    refactoring in data layer for repo impl

--- lib/features/auth/data/repository/auth_repository_implementation.dart
+++ lib/features/auth/data/repository/auth_repository_implementation.dart
@@ -4,11 +4,24 @@ import 'package:blogs_app/features/auth/data/sources/auth_remote_data_source.dar
 import 'package:blogs_app/features/auth/domain/entities/user.dart';
 import 'package:blogs_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:fpdart/src/either.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' as sb;
 
 class AuthRepositoryImplementation implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
 
   const AuthRepositoryImplementation({required this.remoteDataSource});
+
+  /// Wrapper function for login and sign -> refactoring
+  Future<Either<Failures, User>> _getUser(Future<User> Function() fn) async {
+    try {
+      final userId = await fn();
+      return right(userId);
+    } on sb.AuthException catch (e) {
+      return left(Failures(e.message));
+    } on ServerException catch (e) {
+      return left(Failures(e.message));
+    }
+  }
 
   @override
   Future<Either<Failures, User>> currentUser() async {
@@ -29,15 +42,12 @@ class AuthRepositoryImplementation implements AuthRepository {
     required String email,
     required String password,
   }) async {
-    try {
-      final userId = await remoteDataSource.loginWithEmailPassword(
+    return _getUser(
+      () async => await remoteDataSource.loginWithEmailPassword(
         email: email,
         password: password,
-      );
-      return right(userId);
-    } on ServerException catch (e) {
-      return left(Failures(e.message));
-    }
+      ),
+    );
   }
 
   @override
@@ -46,15 +56,12 @@ class AuthRepositoryImplementation implements AuthRepository {
     required String email,
     required String password,
   }) async {
-    try {
-      final userId = await remoteDataSource.signUpWithEmailPassword(
+    return _getUser(
+      () async => await remoteDataSource.signUpWithEmailPassword(
         name: name,
         email: email,
         password: password,
-      );
-      return right(userId);
-    } on ServerException catch (e) {
-      return left(Failures(e.message));
-    }
+      ),
+    );
   }
 }

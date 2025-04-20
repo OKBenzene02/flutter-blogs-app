c237489922082a5c28dcac7ec8c367402ba22e82
OKBenzene02 <liyakhatyousuf@gmail.com>
2025-04-20 16:49:04 +0530

    added text fields for new blog page

--- lib/features/blogs/presentation/pages/add_new_blog.dart
+++ lib/features/blogs/presentation/pages/add_new_blog.dart
@@ -1,4 +1,5 @@
 import 'package:blogs_app/core/theme/app_pallet.dart';
+import 'package:blogs_app/features/blogs/presentation/widgets/blog_editor.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
@@ -13,6 +14,16 @@ class AddNewBlogPage extends StatefulWidget {
 }
 
 class _AddNewBlogPageState extends State<AddNewBlogPage> {
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+
+  @override
+  void dispose() {
+    titleController.dispose();
+    contentController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,64 +36,75 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            DottedBorder(
-              color: AppPallete.borderColor,
-              dashPattern: const [10, 4],
-              radius: const Radius.circular(12),
-              borderType: BorderType.RRect,
-              strokeCap: StrokeCap.round,
-              child: Container(
-                height: 150,
-                width: double.infinity,
-                child: const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(
-                      Icons.folder_open_rounded,
-                      size: 40,
-                    ),
-                    SizedBox(
-                      height: 15,
-                    ),
-                    Text(
-                      'Select your image',
-                      style: TextStyle(
-                        fontSize: 16,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              DottedBorder(
+                color: AppPallete.borderColor,
+                dashPattern: const [10, 4],
+                radius: const Radius.circular(12),
+                borderType: BorderType.RRect,
+                strokeCap: StrokeCap.round,
+                child: Container(
+                  height: 150,
+                  width: double.infinity,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.folder_open_rounded,
+                        size: 40,
                       ),
-                    )
-                  ],
+                      SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        'Select your image',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  'Technology',
-                  'Business',
-                  'Programming',
-                  'Entertainment',
-                ]
-                    .map(
-                      (tag) => Padding(
-                        padding: const EdgeInsets.symmetric(
-                            vertical: 8, horizontal: 5),
-                        child: Chip(
-                          label: Text(tag),
-                          side: const BorderSide(
-                            color: AppPallete.borderColor,
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    'Technology',
+                    'Business',
+                    'Programming',
+                    'Entertainment',
+                  ]
+                      .map(
+                        (tag) => Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 8, horizontal: 5),
+                          child: Chip(
+                            label: Text(tag),
+                            side: const BorderSide(
+                              color: AppPallete.borderColor,
+                            ),
                           ),
                         ),
-                      ),
-                    )
-                    .toList(),
+                      )
+                      .toList(),
+                ),
               ),
-            )
-          ],
+              const SizedBox(
+                height: 8,
+              ),
+              BlogEditor(controller: titleController, hintText: 'Blog title'),
+              const SizedBox(
+                height: 12,
+              ),
+              BlogEditor(
+                  controller: contentController, hintText: 'Blog content')
+            ],
+          ),
         ),
       ),
     );

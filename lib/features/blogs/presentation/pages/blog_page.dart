2621ea50e933e8dba2461aa6f376a7af872569d1
OKBenzene02 <liyakhatyousuf@gmail.com>
2025-04-23 23:10:06 +0530

    added list view builder for displaying blogs

--- lib/features/blogs/presentation/pages/blog_page.dart
+++ lib/features/blogs/presentation/pages/blog_page.dart
@@ -1,9 +1,24 @@
+import 'package:blogs_app/core/common/loader.dart';
+import 'package:blogs_app/core/utils/show_snackbar.dart';
+import 'package:blogs_app/features/blogs/presentation/bloc/blog_bloc.dart';
 import 'package:blogs_app/features/blogs/presentation/pages/add_new_blog.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class BlogPage extends StatelessWidget {
+class BlogPage extends StatefulWidget {
   static route() => MaterialPageRoute(builder: (context) => const BlogPage());
   const BlogPage({super.key});
+
+  @override
+  State<BlogPage> createState() => _BlogPageState();
+}
+
+class _BlogPageState extends State<BlogPage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<BlogBloc>().add(BlogsGetEvent());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +35,30 @@ class BlogPage extends StatelessWidget {
             ),
           ),
         ],
+      ),
+      body: BlocConsumer<BlogBloc, BlogState>(
+        listener: (context, state) {
+          if (state is BlogFailure) {
+            ShowSnackBar(context, state.error);
+          }
+        },
+        builder: (context, state) {
+          if (state is BlogLoading) {
+            return const Loader();
+          }
+
+          if (state is BlogDisplaySuccess) {
+            return ListView.builder(
+              itemCount: state.blogs.length,
+              itemBuilder: (context, index) {
+                final blog = state.blogs[index];
+                return Text(blog.title);
+              },
+            );
+          }
+
+          return const SizedBox.shrink();
+        },
       ),
     );
   }

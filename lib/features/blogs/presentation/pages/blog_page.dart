3dbf101723179b2fbe04b24ff858e859256c05f7
OKBenzene02 <liyakhatyousuf@gmail.com>
2025-04-24 23:58:34 +0530

    added card view for blogs

--- lib/features/blogs/presentation/pages/blog_page.dart
+++ lib/features/blogs/presentation/pages/blog_page.dart
@@ -1,7 +1,9 @@
 import 'package:blogs_app/core/common/loader.dart';
+import 'package:blogs_app/core/theme/app_pallet.dart';
 import 'package:blogs_app/core/utils/show_snackbar.dart';
 import 'package:blogs_app/features/blogs/presentation/bloc/blog_bloc.dart';
 import 'package:blogs_app/features/blogs/presentation/pages/add_new_blog.dart';
+import 'package:blogs_app/features/blogs/presentation/widgets/blog_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -25,6 +27,7 @@ class _BlogPageState extends State<BlogPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Blog App')),
+        centerTitle: true,
         actions: [
           IconButton(
             onPressed: () {
@@ -52,7 +55,14 @@ class _BlogPageState extends State<BlogPage> {
               itemCount: state.blogs.length,
               itemBuilder: (context, index) {
                 final blog = state.blogs[index];
-                return Text(blog.title);
+                return BlogCard(
+                  blog: blog,
+                  color: index % 3 == 0
+                      ? AppPallete.gradient1
+                      : (index % 3 == 1
+                          ? AppPallete.gradient2
+                          : AppPallete.gradient3),
+                );
               },
             );
           }

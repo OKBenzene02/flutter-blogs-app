c1c28885a4339af23e31498ea223f519cae89fea
OKBenzene02 <liyakhatyousuf@gmail.com>
2025-04-26 10:16:08 +0530

    add internet connection check in repo impl

--- lib/features/auth/data/repository/auth_repository_implementation.dart
+++ lib/features/auth/data/repository/auth_repository_implementation.dart
@@ -1,5 +1,7 @@
 import 'package:blogs_app/core/error/exceptions.dart';
 import 'package:blogs_app/core/error/failures.dart';
+import 'package:blogs_app/core/network/connection_checker.dart';
+import 'package:blogs_app/features/auth/data/models/user_model.dart';
 import 'package:blogs_app/features/auth/data/sources/auth_remote_data_source.dart';
 import 'package:blogs_app/core/common/entities/user.dart';
 import 'package:blogs_app/features/auth/domain/repository/auth_repository.dart';
@@ -8,12 +10,17 @@ import 'package:supabase_flutter/supabase_flutter.dart' as sb;
 
 class AuthRepositoryImplementation implements AuthRepository {
   final AuthRemoteDataSource remoteDataSource;
-
-  const AuthRepositoryImplementation({required this.remoteDataSource});
+  final ConnectionChecker connectionChecker;
+  const AuthRepositoryImplementation(
+      {required this.remoteDataSource, required this.connectionChecker});
 
   /// Wrapper function for login and sign -> refactoring
   Future<Either<Failures, User>> _getUser(Future<User> Function() fn) async {
     try {
+      if (!await (connectionChecker.isConnected)) {
+        return left(Failures('No internet connection!'));
+      }
+
       final userId = await fn();
       return right(userId);
     } on sb.AuthException catch (e) {
@@ -26,6 +33,20 @@ class AuthRepositoryImplementation implements AuthRepository {
   @override
   Future<Either<Failures, User>> currentUser() async {
     try {
+      if (!await (connectionChecker.isConnected)) {
+        final session = remoteDataSource.currentUserSession;
+
+        if (session == null) {
+          return left(Failures('User not logged in'));
+        }
+
+        return right(UserModel(
+          id: session.user.id,
+          name: '',
+          email: session.user.email ?? '',
+        ));
+      }
+
       final user = await remoteDataSource.getCurrentUserData();
       if (user != null) {
         return right(user);
